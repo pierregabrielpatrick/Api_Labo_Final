6ea30a7ae01bb84ca87f91005171d6981a93f8c6
pierregabrielpatrick <p.glineur@stag.technofuturtic.education>
2025-09-11 20:06:37 +0200

    initialisation vol 02

---
+++ Api_Labo_Final/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Labo_Final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly MyDbContext _context;
+        private readonly JwtUtils _jwtUtils;
+
+        public AuthController(MyDbContext context, JwtUtils jwtUtils)
+        {
+            _context = context;
+            _jwtUtils = jwtUtils;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterFormDTO form)
+        {
+            if (_context.Users.Any(u => u.Username == form.Username))
+            {
+                return BadRequest(new { Content = $"User with username {form.Username} already exist" });
+            }
+
+            User user = form.ToUser();
+            user.Password = Argon2.Hash(form.Password);
+            user.Role = UserRole.USER;
+
+            _context.Users.Add(user);
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost("login")]
+        public ActionResult<UserTokenDTO> Login([FromBody] LoginFormDTO form)
+        {
+            User? user = _context.Users.FirstOrDefault(u => u.Username == form.Username);
+
+            if (user == null)
+            {
+                return BadRequest(new { Content = $"User with username {form.Username} doesn't exist" });
+            }
+
+            if (!Argon2.Verify(user.Password, form.Password))
+            {
+                return BadRequest(new { Content = "Wrong password" });
+            }
+
+            UserDTO dto = user.ToUserDTO();
+            string token = _jwtUtils.GenerateToken(user);
+
+            UserTokenDTO result = new UserTokenDTO()
+            {
+                User = dto,
+                Token = token
+            };
+
+            return Ok(result);
+        }
+    }
---
+++ Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public UserRole Role { get; set; }
+
+        public List<House> Houses { get; set; } = [];
+    }
+}

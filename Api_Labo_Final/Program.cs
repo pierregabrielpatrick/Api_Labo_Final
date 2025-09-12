abf95d68f5d9511b81f9bee909b3857d626b9ad8
pierregabrielpatrick <p.glineur@stag.technofuturtic.education>
2025-09-12 14:30:33 +0200

    use of fullfillling the program.cs

--- Api_Labo_Final/Program.cs
+++ Api_Labo_Final/Program.cs
@@ -1,8 +1,13 @@
 
 using Api_Labo_Final.Utils;
 using BusinessLogicLayer;
+using Dal;
 using Dal.context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace Api_Labo_Final
 {
@@ -26,6 +31,68 @@ namespace Api_Labo_Final
 
             builder.Services.AddScoped<HouseService>();
             builder.Services.AddScoped<HouseRepository>();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProject", Version = "v1.0.0" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+
+                c.AddSecurityRequirement(securityRequirement);
+
+            });
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                        builder.Configuration.GetSection("TokenInfo").GetSection("secret").Value!)),
+                    ValidateIssuer = false,
+                    ValidIssuer = builder.Configuration.GetSection("TokenInfo").GetSection("issuer").Value!,
+                    ValidateAudience = false,
+                    ValidAudience = builder.Configuration.GetSection("TokenInfo").GetSection("audience").Value!,
+                    ValidateLifetime = false
+                };
+            });
+
+            builder.Services.AddAuthorization(o =>
+            {
+                o.AddPolicy("Auth", p => p.RequireAuthenticatedUser());
+                o.AddPolicy("Admin", p => p.RequireRole("Admin"));
+            });
+
+            builder.Services.AddCors(o =>
+            {
+                o.AddDefaultPolicy(p =>
+                {
+                    p.WithOrigins(
+                        "http://localhost:4200",
+                        builder.Configuration.GetSection("TokenInfo")
+                            .GetSection("audience").Value!);
+                    p.AllowAnyHeader();
+                    p.AllowAnyMethod();
+                });
+            });
 
             var app = builder.Build();
 
